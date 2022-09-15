7186303a49698c16c5f7dd0495992819af7503f1
imashnake0 <kamaleshpaluru07@gmail.com>
2022-09-15 00:05:49 -0400

    Removed boilerplate
    Came with the new project.

--- app/src/main/java/com/imashnake/dank/MainActivity.kt
+++ app/src/main/java/com/imashnake/dank/MainActivity.kt
@@ -7,9 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.imashnake.dank.ui.theme.DankTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,22 +19,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Text(text = "Hello World!")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DankTheme {
-        Greeting("Android")
     }
 }
